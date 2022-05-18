e4052aa5f1fd257362f33eb2e1475ca0be17ad3a
19deva7@mail.ru <S2Ms6Zp18>
2022-05-18 17:18:04 +0300

    add db mysql

--- src/main/java/com/news/news/controllers/NewsController.java
+++ src/main/java/com/news/news/controllers/NewsController.java
@@ -1,15 +1,22 @@
 package com.news.news.controllers;
 
+import com.news.news.models.News;
+import com.news.news.repo.NewsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NewsController {
+    @Autowired
+    private NewsRepository newsRepository;
 
     @GetMapping("/news")
     public String news (Model model){
         model.addAttribute("news", "Новости");
+        Iterable<News> news = newsRepository.findAll();
+        model.addAttribute("allnews", news);
         return "news";
     }
 
