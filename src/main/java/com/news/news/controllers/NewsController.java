76a82d9aee01fd65b60ff55181cc0088e621a911
19deva7@mail.ru <S2Ms6Zp18>
2022-05-19 16:48:35 +0300

    changes

--- src/main/java/com/news/news/controllers/NewsController.java
+++ src/main/java/com/news/news/controllers/NewsController.java
@@ -25,32 +25,53 @@ public class NewsController {
         model.addAttribute("allnews", news);
         return "news";
     }
-
     @GetMapping("/news/add")
     public String newsAdd (Model model){
         return "news-add";
     }
-
     @PostMapping("/news/add")
-    public String newsPostAdd (@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model){
+    public String newsNewsAdd (@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model){
         News news = new News(title, anons, full_text);
         newsRepository.save(news);
         return "redirect:/news";
     }
-
     @GetMapping("/news/{id}")
     public String newsDetails (@PathVariable(value = "id") long id, Model model){
-        Optional <News> news = newsRepository.findById(id);
-        ArrayList<News> result = new ArrayList<>();
-        news.ifPresent(result::add);
-        model.addAttribute("news", result);
-        return "news-details";
+        if (!newsRepository.existsById(id)){
+            return "redirect:/news";
+        }else{
+            Optional <News> news = newsRepository.findById(id);
+            ArrayList<News> result = new ArrayList<>();
+            news.ifPresent(result::add);
+            model.addAttribute("news", result);
+            return "news-details";
+        }
     }
-
-    @GetMapping("/news/{id}/remove")
-    public String newsRemove (@PathVariable(value = "id") long id, Model model){
+    @GetMapping("/news/{id}/edit")
+    public String newsEdit (@PathVariable(value = "id") long id, Model model){
+        if (!newsRepository.existsById(id)){
+            return "redirect:/news";
+        }else{
+            Optional <News> news = newsRepository.findById(id);
+            ArrayList<News> result = new ArrayList<>();
+            news.ifPresent(result::add);
+            model.addAttribute("news", result);
+            return "news-edit";
+        }
+    }
+    @PostMapping("/news/{id}/edit")
+    public String newsUpdate (@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model){
+        News news = newsRepository.findById(id).orElseThrow();
+        news.setTitle(title);
+        news.setAnons(anons);
+        news.setFull_text(full_text);
+        newsRepository.save(news);
+        return "redirect:/news";
+    }
+    @PostMapping("/news/{id}/remove")
+    public String newsDelete (@PathVariable(value = "id") long id, Model model){
         newsRepository.deleteById(id);
-        return "news";
+        return "redirect:/news";
     }
 
 }
