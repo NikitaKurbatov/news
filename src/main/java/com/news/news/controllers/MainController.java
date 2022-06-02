c32768ae5dfea959a66906826b073a76c9f42b8f
19deva7@mail.ru <S2Ms6Zp18>
2022-06-02 15:27:16 +0300

    add table price + jsoup

--- src/main/java/com/news/news/controllers/MainController.java
+++ src/main/java/com/news/news/controllers/MainController.java
@@ -1,15 +1,43 @@
 package com.news.news.controllers;
 
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
+
 @Controller
 public class MainController {
+    private Document doc;
+    private Thread secThread;
+    private Runnable runnable;
 
+    private void init(){
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                getWeb();
+            }
+        };
+        secThread = new Thread(runnable);
+        secThread.start();
+    }
+    private void getWeb(){
+        try {
+            doc = Jsoup.connect("https://yandex.ru/").get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
     @GetMapping("/")
     public String startHome(Model model) {
         model.addAttribute("home", "Главная страница");
+        init();
+        //model.addAttribute("euro", doc.title());
         return "home";
     }
 
