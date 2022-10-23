89c4af04520ab4d73d38ce434199ee01bf94ee0f
Nikita Kurbatov <19deva7@mail.ru>
2022-10-23 12:10:08 +0300

    small reworks

--- src/main/java/com/news/news/NewsApplication.java
+++ src/main/java/com/news/news/NewsApplication.java
@@ -1,5 +1,7 @@
 package com.news.news;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +9,9 @@ import static com.news.news.Config.OpenBrowser.openHomePage;
 
 @SpringBootApplication
 public class NewsApplication {
-
+	static final Logger log = LoggerFactory.getLogger(NewsApplication.class);
 	public static void main(String[] args) {
+		log.info("Before Starting application");
 		SpringApplication.run(NewsApplication.class, args);
 //		SpringApplication.run(SecuringWebApplication.class, args);
 //		openHomePage();
